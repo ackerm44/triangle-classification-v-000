85d58cf30bf8300addd107d0d73eb369d4997844
Amanda Ackerman <ackerm44@gmail.com>
2017-11-05 19:31:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,13 +15,9 @@ class Triangle
     elsif @a != @b && @a != @c && @b != @c
       :scalene
     elsif @a <= 0 || @b <= 0 || @c <= 0
-      begin
         raise TriangleError
-      end
     elsif @a + @b < @c || @a + @c < @b || @b + @c < @a
-      begin
         raise TriangleError
-      end
     end
   end
 
