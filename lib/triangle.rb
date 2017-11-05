fd0d63a35dc6076d03cb558f04ecf8a5d1f25372
Amanda Ackerman <ackerm44@gmail.com>
2017-11-05 19:42:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
     if @a <= 0 || @b <= 0 || @c <= 0
       raise TriangleError
     elsif @a + @b < @c || @a + @c < @b || @b + @c < @a
-      raise Triangle Error
+      raise TriangleError
     elsif @a == @b && @a == @c
       :equilateral
     elsif @a == @b || @b == @c || @a == @c
